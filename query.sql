6428802448a0a0ed9e494ba2f2c2f7bfe8badfbb
SHREYA MHATRE <129739958+ShreyaMhatre@users.noreply.github.com>
2023-07-12 18:25:08 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,27 @@
+use mydata;
+
+CREATE TABLE employeel ( Department VARCHAR(45) NULL,
+
+Name VARCHAR(45) NULL,
+
+Designition VARCHAR(45) NULL,
+
+Email VARCHAR(45) NULL,
+
+Address VARCHAR(45) NULL,
+
+DOB VARCHAR(45) NULL,
+
+DOJ VARCHAR(45) NULL,
+
+id  int Primary Key auto_increment,
+
+Gender VARCHAR(45) NULL,
+
+Phone VARCHAR(45) NULL,
+
+Country VARCHAR(45) NULL,
+
+Salary VARCHAR(45) NULL );
+
+commit;
